1a67d59a8fcf7d4029cd0dc638a2e14e0460a569
Angelica Osianto <138353527+AngelicaOsianto@users.noreply.github.com>
2026-03-14 22:20:51 +0800

    ubah directory

--- redirect.php
+++ redirect.php
@@ -29,7 +29,7 @@ if (file_exists($env_file)) {
 }
 
 // ── KONFIGURASI ──────────────────────────────────────────────
-$TKI_VALIDATE_URL   = $_ENV['TKI_VALIDATE_URL']   ?? 'https://api.stg.solusinegeri.com/gateway/api/authentication/one-time-access-token_with_auth/validate';
+$TKI_VALIDATE_URL   = $_ENV['TKI_VALIDATE_URL']   ?? 'https://api.solusinegeri.com/gateway/api/authentication/one-time-access-token/:userType/validate';
 $TKI_API_KEY        = $_ENV['TKI_API_KEY']        ?? '';
 $TKI_BASIC_USERNAME = $_ENV['TKI_BASIC_USERNAME'] ?? '';
 $TKI_BASIC_PASSWORD = $_ENV['TKI_BASIC_PASSWORD'] ?? '';
